3679326c9874a9f0a7101730fcb0ba6907483e77
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-10-08 11:00:08 +0500

    Add 1st folder

---
+++ Add  two Numbers/index.js
@@ -0,0 +1,13 @@
+// programm to add to Numbers
+// static Numbers
+
+let num1 = 5;
+let num2 = 10;
+let total = num1 + num2;
+console.log("The total of  " + num1 + " and " + num2 + " is: " + total); // Output: The total is: 15
+
+// Dynamic Numbers
+let a  = prompt("Enter first number: ");
+let b  = prompt("Enter second number: ");
+let c = parseInt(a) + parseInt(b);
+console.log("The total of  " + a + " and " + b + " is: " + c); // Output: The total is: 15
