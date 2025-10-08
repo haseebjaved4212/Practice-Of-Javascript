3020f2782177759d20a5e9b8ef4300a461fb5f92
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-10-08 11:10:35 +0500

    Add 4th folder

---
+++ Find Square  Root/index.js
@@ -0,0 +1,7 @@
+// Programm to find a Square Root of a Number
+
+// To find the square root of a number in JavaScript, we can use the Math.sqrt() method.
+
+let a = prompt("Enter a number ");
+let b = Math.sqrt(a);
+console.log("Square root of " + a + " is " + b);
